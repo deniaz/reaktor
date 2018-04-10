0243e959cf858d4a6c8b3b620a22c178978875b7
Robert Vogt <robert@smartive.ch>
2018-04-10 18:52:40 +0200

    Restart

--- src/reducers/example.js
+++
@@ -1,6 +0,0 @@
-const initialState = {};
-
-const handlers = {};
-
-export default (state = initialState, action) =>
-  (handlers[action.type] ? handlers[action.type](state, action) : state);
--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { applyMiddleware, createStore, compose } from 'redux';
-import thunk from 'redux-thunk';
-import combinedReducers from './reducers';
-
-// eslint-disable-next-line no-underscore-dangle,no-undef
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-export default createStore(combinedReducers, composeEnhancers(applyMiddleware(thunk)));
